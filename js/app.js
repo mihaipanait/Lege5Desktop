2506c30a87e6bb6d3455a94ef008c2ed090399b3
Mihai Panait <panait_mihai@yahoo.com>
2014-08-25 14:44:58 +0300

    test location ng

--- js/app.js
+++ js/app.js
@@ -1,15 +1,15 @@
 var app = angular.module("app",[]);
 
-app.controller("classCtrl",function($scope, $timeout){
+app.controller("classCtrl",function($scope, $timeout, $location){
 
-	$scope.sections = [{ "name":"one", "class":"", "show": true }, 
-	{ "name":"two", "show": true },
-	{ "name":"three", "show": true },
-	{ "name":"four", "show": true },
-	{ "name":"oneDetails", "show": false }, 
-	{ "name":"twoDetails", "show": false },
-	{ "name":"threeDetails", "show": false },
-	{ "name":"fourDetails", "show": false }];
+	$scope.sections = [{ "name":"one", "show": true }, 
+						{ "name":"two", "show": true },
+						{ "name":"three", "show": true },
+						{ "name":"four", "show": true },
+						{ "name":"oneDetails", "show": false }, 
+						{ "name":"twoDetails", "show": false },
+						{ "name":"threeDetails", "show": false },
+						{ "name":"fourDetails", "show": false }];
 
 	$scope.changeClass = function(senderSection){
 		if (senderSection.name === "one"){
@@ -18,6 +18,10 @@ app.controller("classCtrl",function($scope, $timeout){
 			$scope.sections[2].show = false;
 			$scope.sections[3].show = false;
 			$scope.sections[4].show = true;
+
+			//$location.path('/details').search({tab: 1});
+			console.log($location.path());
+			$location.search({service: 1});
 		}
 		else if (senderSection.name === "two"){
 			$scope.sections[0].show = false;   
