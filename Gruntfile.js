dffe9061ce83d28aaf51335839f31e8fd10b7d9d
Mihai Panait <panait_mihai@yahoo.com>
2014-09-12 18:36:04 +0300

    fixing ie8

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,9 +13,34 @@ module.exports = function(grunt) {
 				files: '**/*.scss',
 				tasks: ['sass']
 			}
-		}
+		},
+
+		//grunt http-server:dev
+		'http-server': {
+        	'dev': {
+
+            	// the server root directory
+            	root: '',
+
+            	port: 8282,
+            	// port: function() { return 8282; }
+
+            	host: "127.0.0.1",
+
+            	cache: 0,
+            	showDir : true,
+            	autoIndex: true,
+
+            	// server default file extension
+            	ext: "html",
+
+            	// run in parallel with other tasks
+            	runInBackground: false
+        	}
+    	}
 	});
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-http-server');
 	grunt.registerTask('default',['watch']);
 }
